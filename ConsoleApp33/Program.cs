da9ae87ac56839bc83cad8c560b94980220e12da
Melisa Kılıç <mk_i3@hotmail.com>
2024-10-28 22:17:03 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace metotlar_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //out parametreler
+            string sayi = "999";
+            int outSayi;
+            bool sonuc = int.TryParse(sayi, out outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonuc);
+            Console.WriteLine(toplamSonuc);
+
+            //Metot aşırı yükleme - overloading
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+            instance.EkranaYazdir("Ahmet", "Mehmet");
+
+            //Metot imzası
+            //MetotAdı + Parametre Sayısı + Parametre
+            
+
+
+
+
+
+        }
+
+    }
+
+
+    class Metotlar
+    {
+
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+
+
+
+    }   }
